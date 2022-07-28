0dddbe1cd029bea350e1e4721f369a07b190ef6e
WillFrame <32850788+WillFrame@users.noreply.github.com>
2022-07-29 00:06:15 +0300

    Add files via upload

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Footer() {
     return (
@@ -7,9 +6,9 @@ function Footer() {
             <h4 className="footer__info">Учебный проект Яндекс.Практикум х BeatFilm.</h4>
             <div className="footer__bottom-container">
                 <div className="footer__links-container">
-                    <Link className="footer__link" to="#">Яндекс.Практикум</Link>
-                    <Link className="footer__link" to="#">Github</Link>
-                    <Link className="footer__link" to="#">Facebook</Link>
+                    <a className="footer__link" href="https://practicum.yandex.ru/" target='_blank' rel='noreferrer'>Яндекс.Практикум</a>
+                    <a className="footer__link" href="https://github.com/WillFrame" target='_blank' rel='noreferrer'>Github</a>
+                    <a className="footer__link" href="http://facebook.com/" target='_blank' rel='noreferrer'>Facebook</a>
                 </div>
                 <p className="footer__copyright">© 2020</p>
             </div>
