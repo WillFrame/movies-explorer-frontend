1893d2d88c670f243e3f1b8f80568bd8066fb46a
WillFrame <32850788+WillFrame@users.noreply.github.com>
2022-07-28 21:16:08 +0300

    Add files via upload

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Portfolio() {
     return (
         <section className="portfolio">
             <h3 className="portfolio-title">Портфолио</h3>
-            <Link className="portfolio-link" to='#'>
+            <a className="portfolio-link" href='https://github.com/WillFrame/russian-travel' target='_blank' rel='noreferrer'>
                 <h3 className="portfolio-link-text">Статичный сайт</h3>
                 <h3 className="portfolio-pointer">↗</h3>
-            </Link>
-            <Link className="portfolio-link" to='#'>
+            </a>
+            <a className="portfolio-link" href='https://github.com/WillFrame/how-to-learn' target='_blank' rel='noreferrer'>
                 <h3 className="portfolio-link-text">Адаптивный сайт</h3>
                 <h3 className="portfolio-pointer">↗</h3>
-            </Link>
-            <Link className="portfolio-link" to='#'>
+            </a>
+            <a className="portfolio-link" href='https://github.com/WillFrame/react-mesto-api-full' target='_blank' rel='noreferrer'>
                 <h3 className="portfolio-link-text">Одностраничное приложение</h3>
                 <h3 className="portfolio-pointer">↗</h3>
-            </Link>
+            </a>
         </section>
     )
 }
