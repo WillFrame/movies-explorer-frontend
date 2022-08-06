d765e74f1eff3c75954a020494948ce335953d52
WillFrame <32850788+WillFrame@users.noreply.github.com>
2022-08-06 10:46:50 +0300

    Add files via upload

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,38 +1,70 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import MoviesCard from "../MoviesCard/MoviesCard";
 import Preloader from "../Preloader/Preloader";
 
 function MoviesCardList({ movies = [], onLikeMovie, onDeleteMovie, savedMovies = [], setFilteredMovies, isLoading, isMoviesError }) {
+    const [count, setCount] = useState(12);
+    const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
+
     useEffect(() => {
         setFilteredMovies([]);
     }, []);
 
-    console.log(isLoading);
+    useEffect(() => {
+        currentWidth > 1279
+            ? setCount(12)
+            : currentWidth > 767 
+                ? setCount(8)
+                : setCount(5)
+        console.log(currentWidth);
+    }, [currentWidth, movies])
+
+    function addCards() {
+        window.innerWidth > 1279
+            ? setCount(count + 3)
+            : window.innerWidth > 767 
+                ? setCount(count + 2)
+                : setCount(count + 2)
+    }
+
+    useEffect(() => {
+        function setWidth() {
+            setTimeout(() => setCurrentWidth(window.innerWidth), 2000)
+        }
+        window.addEventListener('resize', setWidth);
+        return () => {
+            window.removeEventListener('resize', setWidth);
+        }
+    }, []);
+
 
     return (
         <section className="movies-card-list">
             {isLoading
                 ? <Preloader />
-                : isMoviesError
-                    ? <p className="movies-card-list__error">Произошла ошибка</p>
-                    : <div className="movies-card-list__container">
-                        {movies.map((item) =>
-                            <MoviesCard
-                                title={item.nameRU}
-                                duration={item.duration}
-                                onLikeMovie={onLikeMovie}
-                                onDeleteMovie={onDeleteMovie}
-                                image={item.image.url ? ('https://api.nomoreparties.co/' + item.image.url) : item.image}
-                                id={item.movieId || item.id}
-                                key={item.movieId || item.id}
-                                savedMovies={savedMovies}
-                            />
-                        )}
-                    </div>
-            }
-            { movies.length > 12 
-                ? <button className="movies-card-list__more-button">Ещё</button>
-                : <div className="movies-card-list__margin-bottom"></div>
+                : <>
+                    { isMoviesError
+                        ? <p className="movies-card-list__error">Произошла ошибка</p>
+                        : <div className="movies-card-list__container">
+                            {movies.map((item) =>
+                                <MoviesCard
+                                    title={item.nameRU}
+                                    duration={item.duration}
+                                    onLikeMovie={onLikeMovie}
+                                    onDeleteMovie={onDeleteMovie}
+                                    image={item.image.url ? ('https://api.nomoreparties.co/' + item.image.url) : item.image}
+                                    id={item.movieId || item.id}
+                                    key={item.movieId || item.id}
+                                    savedMovies={savedMovies}
+                                />
+                            ).slice(0, count)}
+                        </div>
+                    }
+                    { movies.length > count
+                        ? <button className="movies-card-list__more-button" type="button" onClick={addCards}>Ещё</button>
+                        : <div className="movies-card-list__margin-bottom"></div>
+                    }
+                </>
             }
         </section>
     )
